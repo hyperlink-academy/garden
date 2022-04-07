d91d0e886b122aed4f6855d31e7cce4d07c2ed5a
Jared Pereira <jared@awarm.space>
2022-04-07 01:54:12 -0400

    add animation to DeckList

--- components/DeckList.tsx
+++ components/DeckList.tsx
@@ -1,7 +1,10 @@
 import { useIndex } from "hooks/useReplicache";
 import { Disclosure, Transition } from "@headlessui/react";
-import { useState } from "react";
+import useMeasure from "react-use-measure";
 import { SmallCard } from "components/SmallCard";
+import { animated, useSpring, useTransition } from "react-spring";
+import { usePrevious } from "hooks/utils";
+import { FindOrCreateCard } from "./FindOrCreateCard";
 
 export const DeckList = () => {
   let decks = useIndex.aev("deck");
@@ -19,72 +22,88 @@ const Deck = (props: { entity: string }) => {
   let title = useIndex.eav(props.entity, "card/title");
   let description = useIndex.eav(props.entity, "card/content");
   let cards = useIndex.eav(props.entity, "deck/contains");
+
   return (
     <div>
       <Disclosure>
-        <Disclosure.Button as="div">
-          <h3 className="text-grey-35 text-xl">{title?.value}</h3>
-          {description?.value}
-        </Disclosure.Button>
-
-        <Transition
-          enter="transition-[max-height] duration-500 ease-out"
-          enterFrom="max-h-2 overflow-hidden"
-          enterTo="max-h-96 overflow-hidden"
-          leave="transition-[max-height] duration-500 ease-out"
-          leaveFrom="max-h-96 overflow-hidden"
-          leaveTo="max-h-2 overflow-hidden"
-        >
-          <Drawer>
-            <SmallCardList cards={cards?.map((c) => c.value.value) || []} />
-          </Drawer>
-        </Transition>
+        {({ open }) => {
+          return (
+            <>
+              <Disclosure.Button as="div">
+                <h3 className="text-grey-35 text-xl">{title?.value}</h3>
+                {description?.value}
+              </Disclosure.Button>
+              <Drawer open={open}>
+                <SmallCardList cards={cards?.map((c) => c.value.value) || []} />
+              </Drawer>
+            </>
+          );
+        }}
       </Disclosure>
     </div>
   );
 };
 
-export const Drawer: React.FC = (props) => {
+export const Drawer: React.FC<{ open: boolean }> = (props) => {
+  const [ref, { height: viewHeight }] = useMeasure();
+  const previousState = usePrevious(props.open);
+  const { height, arrowHeight } = useSpring({
+    from: { height: 0, opacity: 0, arrowHeight: 0 },
+    to: {
+      arrowHeight: props.open ? 100 : 0,
+      height: props.open ? viewHeight : 0,
+    },
+  });
+  console.log(arrowHeight);
   return (
-    <Disclosure.Panel>
-      <div
-        className="bg-bg-drawer"
-        style={{
-          boxShadow:
-            "inset 0px -4px 3px -3px rgba(0, 0, 0, 0.25), inset 0px 9px 6px -6px rgba(0, 0, 0, 0.25)",
-          width: "100%",
-        }}
-      >
+    <animated.div
+      style={{
+        height: props.open && previousState === props.open ? "auto" : height,
+        overflow: "hidden",
+      }}
+    >
+      <Disclosure.Panel static>
         <div
+          className="bg-bg-drawer"
+          ref={ref}
           style={{
-            height: "64px",
-            marginBottom: "-32px",
-            filter:
-              "drop-shadow(0 -1px 3px rgb(0, 0, 0, 0.25)) drop-shadow(0 3px 6px rgb(0, 0, 0, 0.25))",
-            clipPath:
-              "polygon(0 0, 0 100% , 20px 100% ,  36px 100%, 52px 100%, 100% 100%, 100% 0)",
-            overflow: "hidden",
+            boxShadow:
+              "inset 0px -4px 3px -3px rgba(0, 0, 0, 0.25), inset 0px 9px 6px -6px rgba(0, 0, 0, 0.25)",
           }}
         >
           <div
-            className="bg-background mb-2"
             style={{
-              width: "100%",
-              height: "16px",
+              height: "64px",
+              marginBottom: "-32px",
+              filter:
+                "drop-shadow(0 -1px 3px rgb(0, 0, 0, 0.25)) drop-shadow(0 3px 6px rgb(0, 0, 0, 0.25))",
               clipPath:
-                "polygon(0 0, 0 100% , 20px 100% ,  36px 0, 52px 100%, 100% 100%, 100% 0)",
+                "polygon(0 0, 0 100% , 20px 100% ,  36px 100%, 52px 100%, 100% 100%, 100% 0)",
+              overflow: "hidden",
             }}
-          />
+          >
+            <animated.div
+              className="bg-background mb-2"
+              style={{
+                width: "100%",
+                height: "16px",
+                clipPath: arrowHeight.to(
+                  (h) =>
+                    `polygon(0 0, 0 ${h}% , 20px ${h}% ,  36px 0, 52px ${h}%, 100% ${h}%, 100% 0)`
+                ),
+              }}
+            />
+          </div>
+          <div className="pb-4">{props.children}</div>
         </div>
-        <div className="pb-4">{props.children}</div>
-      </div>
-    </Disclosure.Panel>
+      </Disclosure.Panel>
+    </animated.div>
   );
 };
 
 export const SmallCardList = (props: { cards: string[] }) => {
   return (
-    <div>
+    <div className="flex flex-wrap gap-8 p-8">
       {props.cards.map((c) => (
         <SmallCard href="" entityID={c} />
       ))}
