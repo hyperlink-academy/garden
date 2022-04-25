e483ddc656da3823083a77632b7b76a8e188bd51
Brendan Schlagel <brendan.schlagel@gmail.com>
2022-04-25 18:07:44 -0400

    Added next.config.js with scrollRestoration set to true

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  experimental: {
+    scrollRestoration: true,
+  },
+};
+
+module.exports = nextConfig;
