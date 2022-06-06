fdbd4a3b0decb57f441765185ed8da0b1f49ef74
Jared Pereira <jared@awarm.space>
2022-06-06 11:38:49 -0400

    add basic markdown headers

--- components/Textarea/RenderedText.tsx
+++ components/Textarea/RenderedText.tsx
@@ -7,8 +7,18 @@ export const RenderedText = forwardRef<
 >((props, ref) => {
   return (
     <Linkify options={{ className: "text-accent-blue underline" }}>
-      <pre ref={ref} {...props}>
-        {props.text || (
+      <pre ref={ref} {...{ ...props, value: undefined }}>
+        {props.text ? (
+          // One day we should do proper parsing but for now a line-based approach works
+          // great
+          props.text.split("\n").map((t) => {
+            if (t.startsWith("##"))
+              return <p className="font-bold text-grey-35">{t + "\n"}</p>;
+            if (t.startsWith("#"))
+              return <p className="font-bold">{t + "\n"}</p>;
+            return <p>{t + "\n"}</p>;
+          })
+        ) : (
           <span className="!text-grey-80 italic">{props.placeholder}</span>
         )}
       </pre>
