070d27e2840284ec37a890e396b7926a31f9a2aa
Jared Pereira <jared@awarm.space>
2022-08-30 23:01:52 -0400

    add position data to decks

---
+++ backend/SpaceDurableObject/migrations/2022-08-30.ts
@@ -0,0 +1,27 @@
+import { store } from "../fact_store";
+
+export default {
+  date: "2022-08-30",
+  run: async (storage: DurableObjectStorage) => {
+    let fact_store = store(storage, { id: "" });
+    let home = await fact_store.scanIndex.aev("home");
+
+    if (!home[0]) return;
+    let decks = await fact_store.scanIndex.eav(home[0].entity, "deck/contains");
+
+    for (let i = 0; i < decks.length; i++) {
+      await fact_store.assertFact({
+        entity: decks[0].id,
+        attribute: "card/position-in",
+        value: {
+          type: "position",
+          rotation: 0,
+          x: 10,
+          y: i * 300,
+          size: "big",
+        },
+        positions: {},
+      });
+    }
+  },
+};
--- backend/SpaceDurableObject/migrations/index.ts
+++ backend/SpaceDurableObject/migrations/index.ts
@@ -3,6 +3,7 @@ import m2022_07_19 from "./2022-07-19";
 import m2022_07_20 from "./2022-07-20";
 import m2022_08_01 from "./2022-08-01";
 import m2022_08_10 from "./2022-08-10";
+import m2022_08_30 from "./2022-08-30";
 
 export const latestMigration = "2022-07-19";
 export const migrations = [
@@ -11,6 +12,7 @@ export const migrations = [
   m2022_07_20,
   m2022_08_01,
   m2022_08_10,
+  m2022_08_30,
 ].sort((a, b) => {
   return a.date > b.date ? 1 : -1;
 });
