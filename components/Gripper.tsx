a116fb580690d3b5201626bf3229de7668f077f7
celine <celine.park.94@GMAIL.COM>
2022-09-03 00:32:17 -0400

    fixes to member card in cardpreview and issues with padding on gripper and rotate resize handlers

--- components/Gripper.tsx
+++ components/Gripper.tsx
@@ -32,11 +32,12 @@ export const GripperBG = (props: JSX.IntrinsicElements["div"]) => {
   return (
     <div
       {...props}
-      className="h-full w-[6px] mask-repeat-[no-repeat_space]  bg-grey-80 hover:bg-grey-55"
+      className="w-[6px] h-full bg-grey-80 group-hover:bg-grey-55"
       style={{
         WebkitMaskImage: "url(/img/gripper.svg)",
         maskImage: "url(/img/gripper.svg)",
+        maskRepeat: "repeat-y",
       }}
-    ></div>
+    />
   );
 };
