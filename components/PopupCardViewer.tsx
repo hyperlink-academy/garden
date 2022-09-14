aaacfcdbf2d920f40a5df4766b50cc936018da81
celine <celine.park.94@GMAIL.COM>
2022-09-14 17:41:00 -0400

    styled back button and added unwired find or create to the PopUpCardViwer

--- components/PopupCardViewer.tsx
+++ components/PopupCardViewer.tsx
@@ -1,5 +1,8 @@
 import { useContext, useState, createContext, useRef, useEffect } from "react";
 import { CardView } from "./CardView";
+import { GoBackToPage, SearchOrCommand } from "./Icons";
+import { useIndex } from "hooks/useReplicache";
+import { ButtonPrimary, ButtonSecondary } from "./Buttons";
 
 let PopupCardViewerContext = createContext({
   open: (_args: { focused: LinkContextType; entityID: string }) => {},
@@ -26,6 +29,8 @@ export const PopupCardViewer: React.FC = (props) => {
   useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   }, [state.history[0]]);
+
+  let prevCardTitle = useIndex.eav(state.history[1], "card/title");
 
   return (
     <PopupCardViewerContext.Provider
@@ -54,23 +59,42 @@ export const PopupCardViewer: React.FC = (props) => {
 
         <div
           ref={ref}
-          className={`cardWrapper 
+          className={`cardViewerWrapper 
           h-full w-[calc(100vw-16px)] max-w-3xl 
           pt-2 pb-4 sm:pt-4 sm:pb-8 
           shrink-0 sm:shrink        
           focus:outline-none
           snap-center touch-pan-x 
-          flex flex-col items-stretch`}
+          flex flex-col gap-3 items-stretch`}
         >
           {state.history[0] && (
             <>
-              <button
-                onClick={() => {
-                  setState((s) => ({ ...s, history: s.history.slice(1) }));
-                }}
-              >
-                x
-              </button>
+              <div className="cardViewerHeader grid grid-cols-[auto_max-content] items-end gap-4 ">
+                <button
+                  className="CardViewerBackButton overflow-clip"
+                  onClick={() => {
+                    setState((s) => ({ ...s, history: s.history.slice(1) }));
+                  }}
+                >
+                  <div className="grid grid-cols-[max-content_auto] gap-3 items-center place-items-start text-grey-55">
+                    <GoBackToPage />{" "}
+                    <p className="truncate whitespace-nowrap">
+                      {state.history.length >= 2
+                        ? `Back to ${prevCardTitle?.value}`
+                        : "Clear Stack"}{" "}
+                    </p>
+                  </div>
+                </button>
+                <div className="flex items-center">
+                  <div className="lightborder px-3 py-1 italic bg-white border border-grey-80 rounded-l-md text-grey-80">
+                    {" "}
+                    find or create a card...
+                  </div>
+                  <div className="text-accent-blue bg-bg-blue border border-accent-blue  rounded-r-md px-3 py-1">
+                    <SearchOrCommand />
+                  </div>
+                </div>
+              </div>
               <CardView entityID={state.history[0]} />
             </>
           )}
