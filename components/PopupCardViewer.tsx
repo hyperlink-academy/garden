2300d5d3f6649eb39ad3b4e85f957cc9976def06
Brendan Schlagel <brendan.schlagel@gmail.com>
2022-09-18 15:04:26 -0400

    Added empty state help text for CardViewer

--- components/PopupCardViewer.tsx
+++ components/PopupCardViewer.tsx
@@ -6,6 +6,7 @@ import {
   Member,
   SearchOrCommand,
   Card as CardIcon,
+  RightArrow,
 } from "./Icons";
 import { useIndex, useMutations } from "hooks/useReplicache";
 import { ulid } from "src/ulid";
@@ -81,7 +82,7 @@ export const PopupCardViewer: React.FC = (props) => {
           snap-center touch-pan-x 
           flex flex-col gap-3 items-stretch`}
         >
-          {state.history[0] && (
+          {state.history[0] ? (
             <>
               <div className="cardViewerHeader grid grid-cols-[auto_max-content] items-center gap-4 ">
                 <button
@@ -126,6 +127,60 @@ export const PopupCardViewer: React.FC = (props) => {
                 <FindOrCreateBar />
               </div>
               <CardView entityID={state.history[0]} key={state.history[0]} />
+            </>
+          ) : (
+            <>
+              <div className="cardViewerHeader grid items-center gap-4 justify-end">
+                <FindOrCreateBar />
+              </div>
+              {/* merged various nested card wrappers */}
+              <div
+                className={`
+                w-full
+                max-w-3xl mx-auto
+                overflow-y-scroll       
+                relative
+                no-scrollbar
+                snap-y snap-mandatory snap-start
+                border border-dashed border-grey-80 rounded-lg
+                text-grey-35
+                flex flex-col gap-6
+                p-4
+                `}
+              >
+                <h3>Welcome!</h3>
+                <p>
+                  To the <strong>left</strong> is the <strong>Desktop</strong>,
+                  a canvas for working with cards.
+                </p>
+                <ul className="list-disc list-outside ml-4">
+                  <li>
+                    Double click to add a card — regular cards, decks, or chat
+                    cards
+                  </li>
+                  <li>Move, rotate, and resize cards</li>
+                  <li>
+                    Drag a card into a deck, or on top of another card to make a
+                    new deck
+                  </li>
+                </ul>
+                <p>
+                  Here on the <strong>right</strong> is your{" "}
+                  <strong>Card Viewer</strong>.
+                </p>
+                <ul className="list-disc list-outside ml-4">
+                  <li>To open cards, click from the Desktop or search above</li>
+                  <li>Cards will stack temporarily as you browse</li>
+                </ul>
+                <p>
+                  Above the Desktop, view <strong>Space info</strong> and{" "}
+                  <strong>Highlights</strong>.
+                </p>
+                <p>
+                  Click the same icon on any card to add a{" "}
+                  <strong>new Highlight</strong> for others to see.
+                </p>
+              </div>
             </>
           )}
         </div>
