686e9c68a9cf38df2bc46ebdce03ea50329e4406
ssarratea <solquemal@gmail.com>
2022-11-09 16:52:34 +0100

    add solid color canvas

---
+++ components/CardView/GenerativeCanvas.tsx
@@ -0,0 +1,7 @@
+import {  Stage } from "react-pixi-fiber";
+
+export function GenerativeCanvas() {
+  return (
+    <Stage options={{ backgroundColor: 0x10bb99, height: 600, width: 800 }}>
+  </Stage>)
+}
