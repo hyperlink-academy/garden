818bc270800e32194ad7467bd70bbb4d94ad7246
ssarratea <solquemal@gmail.com>
2022-11-10 00:44:05 +0100

    simple example of pixi js component

--- components/CardView/background/PixiJS/Canvas.tsx
+++ components/CardView/background/PixiJS/Canvas.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
+import { Stage } from "react-pixi-fiber";
 
 const width = 600;
 const height = 400;
 
+const options = {
+  backgroundColor: 0x56789a,
+  width: width,
+  height: height
+};
+
+const style = {
+  width: width,
+  height: height
+};
+
 export function Canvas() {
   return (
+    <Stage options={options} style={style}>
+    </Stage>
   );
 }
