53f405dcd0ca6cb909bd37a027dfd5af8837ef72
ssarratea <solquemal@gmail.com>
2022-11-10 00:30:40 +0100

    remove unused file

--- components/CardView/GenerativeCanvas.tsx
+++
@@ -1,7 +0,0 @@
-import {  Stage } from "react-pixi-fiber";
-
-export function GenerativeCanvas() {
-  return (
-    <Stage options={{ backgroundColor: 0x10bb99, height: 600, width: 800 }}>
-  </Stage>)
-}
