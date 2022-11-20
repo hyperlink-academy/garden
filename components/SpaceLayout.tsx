4accbe987e0efafabb7a4c6264f7187ebcdd2a0b
Jared Pereira <jared@awarm.space>
2022-11-20 02:10:40 -0500

    remove ununused components

--- components/SpaceLayout.tsx
+++ components/SpaceLayout.tsx
@@ -1,19 +1,10 @@
 import { useAuth } from "hooks/useAuth";
 import Link from "next/link";
+import useSWR from "swr";
 import { useRouter } from "next/router";
-import {
-  ChatBubble,
-  Information,
-  DeckLarge,
-  ExitDoor,
-  ActivityBlocks,
-  Studio,
-  Settings,
-} from "./Icons";
-import { ButtonLink, ButtonPrimary, ButtonSecondary } from "./Buttons";
-import { useContext, useMemo, useState } from "react";
-import { Modal } from "components/Layout";
-import { LogInModal } from "./LoginModal";
+import { ExitDoor, Settings } from "./Icons";
+import { ButtonSecondary } from "./Buttons";
+import { useContext, useMemo } from "react";
 import { SmallCardDragContext } from "./DragContext";
 import { spacePath } from "hooks/utils";
 import {
@@ -24,13 +15,11 @@ import {
   useSpaceID,
 } from "hooks/useReplicache";
 import { useSubscribe } from "replicache-react";
-import { Message } from "data/Messages";
-import { useInActivity } from "hooks/useInActivity";
 import Head from "next/head";
 import { Fact } from "data/Facts";
-import { Join } from "./SpaceInfo";
 import { Disclosure } from "@headlessui/react";
-import { Drawer } from "./Drawer";
+import { useSmoker } from "./Smoke";
+import { spaceAPI } from "backend/lib/api";
 
 export const SpaceLayout: React.FC = (props) => {
   let spaceName = useIndex.aev("this/name")[0];
@@ -98,305 +87,44 @@ export const SpaceLayout: React.FC = (props) => {
   );
 };
 
-export function Footer() {
-  let router = useRouter();
+const WORKER_URL = process.env.NEXT_PUBLIC_WORKER_URL as string;
+export const Join = () => {
   let { session } = useAuth();
-  let [isOpen, setLogInModal] = useState(false);
-
-  return (
-    <>
-      <style jsx>{`
-        @media print {
-          .menu {
-            display: none;
-          }
+  let isMember = useIndex.ave("space/member", session.session?.studio);
+  let smoker = useSmoker();
+  const spaceID = useSpaceID();
+  let { data: inviteLink } = useSWR(
+    !isMember ? null : `${WORKER_URL}/space/${spaceID}/get_share_code`,
+    async () => {
+      if (!spaceID || !session.token) return;
+      let code = await spaceAPI(
+        `${WORKER_URL}/space/${spaceID}`,
+        "get_share_code",
+        {
+          token: session.token,
         }
-        @media (max-height: 500px) {
-          .menu {
-            display: none;
-          }
-        }
-      `}</style>
-      <div
-        style={{
-          position: "fixed",
-          bottom: "24px",
-          left: "50%",
-          transform: "translateX(-50%)",
-        }}
-        className={`menu z-50 w-full px-4`}
-      >
-        <div className="grid max-w-3xl mx-auto">
-          <div
-            style={{ gridArea: "1 / 1 / 2 / 2" }}
-            className="flex flex-row rounded-md bg-grey-15 -mb-0.5 mt-0.5 -ml-0.5 mr-0.5"
-          />
-          <div
-            style={{ gridArea: "1 / 1 / 2 / 2" }}
-            className="flex flex-row rounded-md bg-white border-2 border-grey-15 px-2 md:px-4 justify-between w-full"
-          >
-            <div className="flex flex-row">
-              {!session?.loggedIn ? (
-                <>
-                  <ButtonLink
-                    className="justify-self-start"
-                    content="Log In"
-                    onClick={() => setLogInModal(true)}
-                  />
-                  <LogInModal
-                    isOpen={isOpen}
-                    onClose={() => setLogInModal(false)}
-                  />
-                </>
-              ) : (
-                <Link href={`/s/${session.session.username}`}>
-                  <div className="justify-self-start flex items-center">
-                    <ExitDoor />
-                  </div>
-                </Link>
-              )}
-
-              <div className="h-full py-1 pl-2 md:pl-4 pr-2 md:pr-3">
-                <svg
-                  width="1"
-                  height="32"
-                  viewBox="0 0 1 32"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <line
-                    x1="0"
-                    y1="4"
-                    x2="0"
-                    y2="28"
-                    stroke="#8C8C8C"
-                    strokeLinecap="round"
-                  />
-                </svg>
-              </div>
-
-              <div className="justify-self-center items-center flex flex-row gap-1 md:gap-2 mr-2">
-                {/* HOME */}
-                {/* NB: TESTING! */}
-                <Home />
-
-                {/* DECKS */}
-                <FooterItem
-                  active={(r) =>
-                    !r.includes("/chat") &&
-                    !r.includes("/activity") &&
-                    !r.includes("/home")
-                  }
-                  route={`${spacePath(
-                    router.query.studio,
-                    router.query.space
-                  )}`}
-                >
-                  <DeckLarge width={32} height={32} />
-                </FooterItem>
-
-                {/* CHAT */}
-                <FooterItem
-                  active={(r) => r.endsWith("chat")}
-                  route={`${spacePath(
-                    router.query.studio,
-                    router.query.space
-                  )}/chat`}
-                >
-                  <ChatIcon />
-                </FooterItem>
-
-                {/* ACTIVITIES */}
-                <Activity />
-              </div>
-            </div>
-
-            {/* INFO */}
-            <InfoModal />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
-
-const Activity = () => {
-  let router = useRouter();
-  let inActivity = useInActivity();
-  let activityName = useIndex.eav(
-    inActivity?.value.value || null,
-    "activity/name"
-  );
-  let selected = router.asPath.includes("/activity");
-  return (
-    <Link
-      href={`/s/${router.query.studio}/s/${router.query.space}/activity${
-        !inActivity ? "" : `/${inActivity.value.value}`
-      }`}
-    >
-      <span
-        className={`px-0.5 md:px-1 grid grid-flow-col ${
-          !selected
-            ? ""
-            : "bg-grey-15 text-white rounded-md relative -top-2 mb-1"
-        }`}
-      >
-        {activityName ? (
-          <small
-            className={`self-baseline mt-[5px] italic col-start-1 row-start-1 ml-4 mr-1 md:mr-0 pl-5 pr-2 whitespace-nowrap overflow-hidden ${
-              !selected ? "bg-grey-90 rounded-r-full " : ""
-            }`}
-          >
-            {activityName?.value}
-          </small>
-        ) : null}
-        <span className="col-start-1 row-start-1">
-          <ActivityBlocks />
-        </span>
-      </span>
-    </Link>
-  );
-};
-
-const FooterItem: React.FC<{
-  route: string;
-  active: (route: string) => boolean;
-}> = (props) => {
-  let router = useRouter();
-  let selected = props.active(router.asPath);
-  return (
-    <Link href={props.route}>
-      <span
-        className={`px-0.5 md:px-1 ${
-          !selected
-            ? ""
-            : "bg-grey-15 text-white rounded-md relative -top-2 mb-1"
-        }`}
-      >
-        {props.children}
-      </span>
-    </Link>
-  );
-};
-
-const ChatIcon = () => {
-  let ctx = useContext(ReplicacheContext);
-  let spaceID = useSpaceID();
-  let { rep: studio } = useAuth();
-  let lastSeen = useSubscribe(
-    studio,
-    async (tx) => {
-      if (!spaceID) return 0;
-      let q = scanIndex(tx);
-      let space = await q.ave("space/id", spaceID);
-      if (!space) return 0;
-      let latest = await q.eav(space.entity, "space/lastSeenMessage");
-      return latest?.value || 0;
-    },
-    0,
-    [spaceID]
-  );
-  let latestMsg = useSubscribe(
-    ctx?.rep,
-    async (tx) => {
-      let messages = await tx
-        .scan({ indexName: "messages" })
-        .values()
-        .toArray();
-      let lastMessage = messages[messages.length - 1] as Message;
-      return lastMessage?.index || 0;
-    },
-    0,
-    []
+      );
+      if (code.success) {
+        return `${document.location.href}/join?code=${code.code}`;
+      }
+    }
   );
 
-  return (
-    <div className="max-h-8">
-      <ChatBubble width={32} height={32} />
-      {lastSeen && latestMsg > lastSeen ? (
-        <div className="bg-accent-red relative rounded-full -top-8 left-5 w-3 h-3"></div>
-      ) : null}
-    </div>
-  );
-};
+  const getShareLink = async (e: React.MouseEvent) => {
+    if (!inviteLink) return;
+    await navigator.clipboard.writeText(inviteLink);
+    smoker({ position: { x: e.clientX, y: e.clientY }, text: "copied!" });
+  };
 
-function InfoModal() {
-  let [isOpen, setInfoModal] = useState(false);
-
-  return (
-    <div className="justify-self-end flex items-center">
-      <button onClick={() => setInfoModal(true)}>
-        <Information />
-      </button>
-
-      <Modal open={isOpen} onClose={() => setInfoModal(false)}>
-        <h2>Hyperlink Info</h2>
-        <p>Looking for help? A few pointers 🌱👇</p>
-        <ul className="list-disc list-inside">
-          <li className="pb-4">
-            <a
-              href="https://hyperlink.garden/s/Hyperlink%20Team/s/Hyperlink%20Handbook"
-              target="_blank"
-              className="text-accent-blue hover:underline decoration-accent-blue"
-            >
-              Hyperlink Handbook
-            </a>
-            : read our in-depth guide to the Hyperlink app
-          </li>
-          <li className="pb-4">
-            <a
-              href="https://hyperlink.garden/s/Hyperlink%20Team/s/Hypernaut%20Hub"
-              target="_blank"
-              className="text-accent-blue hover:underline decoration-accent-blue"
-            >
-              Hypernaut Hub
-            </a>
-            : share bugs, questions, suggestions, or just hang and chat!
-          </li>
-          <li>
-            <a
-              href="https://hyperlink.garden/s/Hyperlink%20Team/s/Activity%20Library"
-              target="_blank"
-              className="text-accent-blue hover:underline decoration-accent-blue"
-            >
-              Activity Library
-            </a>
-            : explore other interesting Spaces for inspiration
-          </li>
-        </ul>
-        <p>
-          You can also{" "}
-          <a
-            href="mailto:contact@hyperlink.academy"
-            target="_blank"
-            className="text-accent-blue hover:underline decoration-accent-blue"
-          >
-            email us
-          </a>{" "}
-          with any private questions or feedback — thanks!
-        </p>
-      </Modal>
-    </div>
-  );
-}
-
-const Home = () => {
-  let router = useRouter();
-
-  // do we have a "home" deck?
-  let deck = useIndex.ave("card/title", "Home");
-
-  // prevent complaints below about maybe null deck prop
-  if (deck === null) return null;
-
-  return (
-    <FooterItem
-      active={(r) => r.endsWith("home")}
-      route={`/s/${router.query.studio}/s/${router.query.space}/home`}
-    >
-      <Studio width={32} height={32} />
-    </FooterItem>
-  );
+  if (isMember && inviteLink) {
+    return (
+      <>
+        <input readOnly value={inviteLink} className="hidden" />
+        <button onClick={getShareLink}> share link</button>
+      </>
+    );
+  }
+  return null;
 };
 
 let useUnreads = () => {
