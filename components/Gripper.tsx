48e18c2c5bb3d8bd82ca6ef4d62a656f90e5d61b
Jared Pereira <jared@awarm.space>
2022-11-23 00:31:03 -0500

    remove unsued gripper

--- components/Gripper.tsx
+++ components/Gripper.tsx
@@ -1,32 +1,3 @@
-import { url } from "inspector";
-
-type SVGProps = JSX.IntrinsicElements["svg"];
-
-// We're not using Gripper anymore in favor of GripperBG.
-// Gripper has a default minimum height of 150pc because it inherits the height from whatever parent is put in.
-// But if the parent is responsive, then it doesn't know it's height. ANyway trust me it doesn't work.
-// We're keeping it since some old components still use it, but plan on removing it eventually
-
-export const Gripper = (props: SVGProps) => {
-  return (
-    <svg width="6" height="100%" className="min-h-2" {...props}>
-      <pattern
-        id="gripper"
-        x="0"
-        y="0"
-        viewBox="0 0 6 4"
-        width="6"
-        height="4"
-        patternUnits="userSpaceOnUse"
-      >
-        <rect width="2" height="2" rx="1" fill="currentColor" />
-        <rect x="4" width="2" height="2" rx="1" fill="currentColor" />
-      </pattern>
-      <rect x="0" y="0" width="6" height="100%" fill="url(#gripper)" />
-    </svg>
-  );
-};
-
 // Here's the correct Gripper
 export const GripperBG = (props: JSX.IntrinsicElements["div"]) => {
   return (
