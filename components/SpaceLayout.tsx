d68ec568b7c380487680bba29166fa680b12911f
celine <celine.park.94@GMAIL.COM>
2022-11-28 20:11:33 -0500

    made more tweaks to header, fixed logged out state to not show invite button, highlight button, or back to studio button

--- components/SpaceLayout.tsx
+++ components/SpaceLayout.tsx
@@ -2,7 +2,7 @@ import { useAuth } from "hooks/useAuth";
 import Link from "next/link";
 import useSWR from "swr";
 import { useRouter } from "next/router";
-import { ExitDoor, MemberAdd } from "./Icons";
+import { ExitDoor, ExpandTiny, Member, MemberAdd } from "./Icons";
 import { SmallCardDragContext } from "./DragContext";
 import { spacePath } from "hooks/utils";
 import { useIndex, useSpaceID } from "hooks/useReplicache";
@@ -10,14 +10,21 @@ import Head from "next/head";
 import { Disclosure } from "@headlessui/react";
 import { useSmoker } from "./Smoke";
 import { spaceAPI } from "backend/lib/api";
-import { Divider } from "./Layout";
+import { Divider, Modal } from "./Layout";
 import { useNextHighlight } from "hooks/useNextHighlight";
+import { useState } from "react";
+import { ButtonPrimary, ButtonSecondary } from "./Buttons";
+import { LogInModal } from "./LoginModal";
 
 export const SpaceLayout: React.FC = (props) => {
-  let spaceName = useIndex.aev("this/name")[0];
-  let highlight = useNextHighlight();
   let { session } = useAuth();
-  let { query } = useRouter();
+  let { query, pathname } = useRouter();
+
+  let spaceName = useIndex.aev("this/name")[0];
+  let newHighlightAvailable = useNextHighlight();
+  let isHighlightPage = pathname.endsWith("/highlights");
+  let [isInviteOpen, setIsOpen] = useState(false);
+  let [isLogInOpen, setLogInOpen] = useState(false);
 
   return (
     <>
@@ -27,64 +34,108 @@ export const SpaceLayout: React.FC = (props) => {
       <div className="pageWrapper flex flex-col sm:gap-4 gap-2 h-screen items-stretch justify-items-center overflow-hidden">
         {/* HEADER START */}
         <div className="pageHeader shrink-0 sm:pb-4 pb-2">
-          <Disclosure>
-            <div
-              className={`
+          <div
+            className={`
                 max-w-6xl mx-auto sm:px-4 px-2 
                 before:content-[''] before:absolute before:w-[100vw] before:h-14 before:left-0 ${
-                  highlight ? "before:bg-accent-blue" : "before:bg-grey-35"
+                  newHighlightAvailable
+                    ? "before:bg-accent-blue"
+                    : "before:bg-grey-35"
                 }`}
-            >
-              <div className="grid grid-cols-[max-content_auto_max-content]  items-end gap-2 pt-3">
-                {!session.session ? (
-                  <div className="w-20" />
-                ) : (
-                  <div className="z-10 w-20 ">
-                    <Link href={`/s/${session.session.username}`}>
-                      <div className="py-1 px-2 w-fit rounded-md text-accent-blue bg-white hover:bg-bg-blue">
-                        <ExitDoor />
-                      </div>
-                    </Link>
-                  </div>
-                )}
-                <div className="text-white font-bold mx-auto text-center flex gap-2 grow z-10">
-                  <h2>{spaceName?.value} </h2>
-                  {window.location.href.endsWith("/highlights") ? (
-                    <Link href={`${spacePath(query.studio, query.space)}`}>
-                      <h4 className="font-normal"> See Highlight </h4>
-                    </Link>
-                  ) : (
-                    <Link
-                      href={`${spacePath(
-                        query.studio,
-                        query.space
-                      )}/highlights`}
+          >
+            <div className="grid grid-cols-[max-content_auto_max-content] items-center gap-2 pt-3">
+              {!session.session ? (
+                <div className="w-20" />
+              ) : (
+                <div className="z-10 w-20 ">
+                  <Link href={`/s/${session.session.username}`}>
+                    <div
+                      className={`                  
+                        ${
+                          newHighlightAvailable
+                            ? "text-accent-blue"
+                            : "text-grey-35"
+                        } py-1 px-2 w-fit rounded-md  bg-white hover:bg-bg-blue`}
                     >
-                      <div className="flex gap-4">
-                        <div className="bg-white text-accent-blue rounded-md py-1 px-2">
-                          Desktop
-                        </div>
-                        <Divider />
-                        <div>Highlight</div>
-                      </div>
-                    </Link>
-                  )}
+                      <ExitDoor />
+                    </div>
+                  </Link>
                 </div>
+              )}
+              <div className="text-white mx-auto text-center grid grid-cols-[max-content_1px_max-content] gap-4 grow  z-10">
+                <h2 className="place-self-center">{spaceName?.value} </h2>
 
-                <div className="z-10 w-20">
-                  <div className="float-right text-white">
-                    <MemberAdd />
-                  </div>
-                </div>
+                {!session.session ? null : (
+                  <>
+                    <Divider />
+
+                    <div
+                      className={`${
+                        newHighlightAvailable
+                          ? "text-accent-blue"
+                          : "text-grey-35"
+                      } flex gap-2 `}
+                    >
+                      <div
+                        className={`${
+                          isHighlightPage
+                            ? `bg-transparent border border-transparent hover:border hover:border-white text-white `
+                            : `bg-white text-inherit font-bold`
+                        } rounded-md py-1 px-2`}
+                      >
+                        <Link href={`${spacePath(query.studio, query.space)}`}>
+                          Desktop
+                        </Link>
+                      </div>
+                      <div
+                        className={`${
+                          isHighlightPage
+                            ? `bg-white text-inherit font-bold`
+                            : `bg-transparent border border-transparent hover:border hover:border-white text-white `
+                        } rounded-md py-1 px-2`}
+                      >
+                        <Link
+                          href={`${spacePath(
+                            query.studio,
+                            query.space
+                          )}/highlights`}
+                        >
+                          Highlight
+                        </Link>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+
+              <div className="z-10 w-20">
+                {!session.session ? (
+                  <>
+                    <ButtonSecondary
+                      content="Log In"
+                      onClick={() => setLogInOpen(!isLogInOpen)}
+                    />
+
+                    <LogInModal
+                      isOpen={isLogInOpen}
+                      onClose={() => setLogInOpen(false)}
+                    />
+                  </>
+                ) : (
+                  <button
+                    className={`text-white float-right`}
+                    onClick={() => setIsOpen(!isInviteOpen)}
+                  >
+                    <Member />
+                  </button>
+                )}
               </div>
             </div>
+          </div>
 
-            <Disclosure.Panel>
-              <div className="bg-white flex flex-row justify-between p-2 border border-accent-blue rounded-b-md">
-                <Join />
-              </div>
-            </Disclosure.Panel>
-          </Disclosure>
+          <Modal open={isInviteOpen} onClose={() => setIsOpen(false)}>
+            <Join />
+          </Modal>
         </div>
         {/* HEADER END */}
 
@@ -109,7 +160,8 @@ const SettingsPanel = () => {
 };
 
 const WORKER_URL = process.env.NEXT_PUBLIC_WORKER_URL as string;
-export const Join = () => {
+
+const Join = () => {
   let { session } = useAuth();
   let isMember = useIndex.ave("space/member", session.session?.studio);
   let smoker = useSmoker();
@@ -139,10 +191,13 @@ export const Join = () => {
 
   if (isMember && inviteLink) {
     return (
-      <>
-        <input readOnly value={inviteLink} className="hidden" />
-        <button onClick={getShareLink}> share link</button>
-      </>
+      <div className="flex flex-col gap-4 text-center">
+        <p className="font-bold">Share this link to invite new members!</p>
+        <div className="w-full flex gap-2">
+          <input className="grow" readOnly value={inviteLink} />
+          <ButtonSecondary onClick={getShareLink} content="Copy Link" />
+        </div>
+      </div>
     );
   }
   return null;
