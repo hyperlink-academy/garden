2a1f5c61902e193b987cbc30f56018964eec8a48
Jared Pereira <jared@awarm.space>
2023-02-10 16:41:23 -0500

    disable sw building in development mode

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 const withPWA = require("next-pwa")({
   dest: "public",
   mode: "production",
+  disable: process.env.NODE_ENV !== "production",
 });
 const nextConfig = {
   /* config options here */
