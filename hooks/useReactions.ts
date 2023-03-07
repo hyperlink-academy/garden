f6602612fa95cb0a5d8bebdca08cdf02e0ce1c4e
Brendan Schlagel <brendan.schlagel@gmail.com>
2023-03-07 17:42:58 -0500

    added new hook useReactions

---
+++ hooks/useReactions.ts
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { useSubscribe } from "replicache-react";
+import { ReplicacheContext, scanIndex, useMutations } from "./useReplicache";
+
+export const useReactions = (entityID: string) => {
+  let { memberEntity } = useMutations();
+  let rep = useContext(ReplicacheContext);
+
+  let reactions = useSubscribe(
+    rep?.rep,
+    async (tx) => {
+      let reactions = await scanIndex(tx).eav(entityID, "card/reaction");
+      let data: {
+        [reaction: string]: { count: number; memberReaction: string | null };
+      } = {};
+      for (let reaction of reactions) {
+        let r = data[reaction.value] || { count: 0, memberReaction: null };
+        if (memberEntity) {
+          let author = await scanIndex(tx).eav(reaction.id, "reaction/author");
+          if (author?.value.value === memberEntity)
+            r.memberReaction = reaction.id;
+        }
+        r.count++;
+        data[reaction.value] = r;
+      }
+      return Object.entries(data);
+    },
+    [],
+    [entityID, memberEntity]
+  );
+  return reactions;
+};
