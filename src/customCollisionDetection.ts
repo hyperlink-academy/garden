c5ee1807296f659966a17bca2021bbb602f09113
Jared Pereira <jared@awarm.space>
2023-03-16 15:28:21 -0400

    only use pointer collision detection

--- src/customCollisionDetection.ts
+++ src/customCollisionDetection.ts
@@ -115,11 +115,12 @@ export function pointerWithinOrRectIntersection(
   // First, let's see if there are any collisions with the pointer
   const pointerCollisions = pointerWithin(args);
 
+  return pointerCollisions;
   // Collision detection algorithms return an array of collisions
-  if (pointerCollisions.length > 0) {
-    return pointerCollisions;
-  }
+  //if (pointerCollisions.length > 0) {
+  //    return pointerCollisions;
+  //}
 
   // If there are no collisions with the pointer, return rectangle intersections
-  return rectIntersection(args);
+  // return rectIntersection(args)
 }
