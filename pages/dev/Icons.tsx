31ce974a4d12d5e8689aaa8d92379d8974835d22
Brendan Schlagel <brendan.schlagel@gmail.com>
2023-04-19 22:52:40 -0400

    fix react missing key

--- pages/dev/Icons.tsx
+++ pages/dev/Icons.tsx
@@ -4,7 +4,7 @@ export default function IconsPage() {
   return (
     <div className="flex flex-wrap gap-4 p-4">
       {Object.values(Icons).map((Icon, index) => (
-        <svg width="64" height="64">
+        <svg key={index} width="64" height="64">
           <title>{Icon.name}</title>
           <Icon key={index} />
         </svg>
