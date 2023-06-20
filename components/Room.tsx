4d1e4458362a1beef32d4635d56e7e1cb1029134
celine <celine.park.94@GMAIL.COM>
2023-06-20 14:14:49 -0400

    refactor the roomheader description toggle

--- components/Room.tsx
+++ components/Room.tsx
@@ -9,6 +9,7 @@ import { useFilteredCards, Filters, FilterByReactions } from "./CardFilter";
 import { Discussion } from "./CardView/Discussion";
 import { Desktop } from "./Desktop";
 import { GoToTop, MoreOptionsTiny } from "./Icons";
+import { Divider } from "./Layout";
 import { SearchRoom } from "./SearchRoom";
 import { EditRoomModal } from "./SpaceLayout/Sidebar/RoomListLayout";
 import { RenderedText } from "./Textarea/RenderedText";
@@ -27,9 +28,9 @@ export const Room = (props: { entityID: string }) => {
 
   return (
     <div
-      id="roomScrollContainer"
+      id="room-wrapper"
       ref={ref}
-      className="no-scrollbar flex h-full w-[336px] flex-col gap-1 overflow-x-hidden overflow-y-scroll p-2 pt-0 text-sm sm:p-4 sm:pt-0 "
+      className="no-scrollbar flex h-full w-[336px] flex-col items-stretch overflow-x-hidden overflow-y-scroll p-2 pt-0 text-sm sm:p-4 sm:pt-0 "
     >
       <RoomHeader
         entityID={props.entityID}
@@ -41,7 +42,7 @@ export const Room = (props: { entityID: string }) => {
       {/* per-room wrappers + components */}
       {props.entityID ? (
         roomType?.value === "collection" ? (
-          <div className="flex min-h-[calc(100vh-132px)] flex-col gap-2">
+          <div className="flex  grow flex-col  gap-2 pb-3">
             <CardCollection
               cards={cardsFiltered}
               entityID={props.entityID}
@@ -53,7 +54,7 @@ export const Room = (props: { entityID: string }) => {
         ) : (
           <div className="relative flex flex-col">
             <Desktop entityID={props.entityID} />
-            <div className="desktopBackground absolute h-full w-full" />
+            <div className="desktopBackground absolute h-full  w-full grow" />
           </div>
         )
       ) : null}
@@ -67,36 +68,28 @@ function RoomHeader(props: {
   filters: Filters;
   setFilters: (f: (old: Filters) => Filters) => void;
 }) {
-  const [isRoomDescriptionVisible, setIsRoomDescriptionVisible] =
-    useState(true);
-  const [
-    isToggleableRoomDescriptionHidden,
-    setIsToggleableRoomDescriptionHidden,
-  ] = useState(false);
-  let [titleRef, { height }] = useMeasure();
+  let [descriptionOpen, setDescriptionOpen] = useState(false);
+  let [scrolledTop, setScrolledTop] = useState(true);
+
+  let [titleRef, { height: titleHeight }] = useMeasure();
+  let [descriptionRef, { height: descriptionHeight }] = useMeasure();
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.target.id === "roomDescription") {
-          setIsRoomDescriptionVisible(entry.isIntersecting);
-          setIsToggleableRoomDescriptionHidden(true);
-        }
-      });
-    });
-    let roomDescription = document.getElementById("roomDescription");
-    if (!roomDescription) return;
-    observer.observe(roomDescription);
+    let roomWrapper = document.getElementById("room-wrapper");
+    const onScroll = (event: Event) => {
+      if (roomWrapper?.scrollTop === 0) {
+        setScrolledTop(true);
+      } else {
+        setScrolledTop(false);
+      }
+    };
+
+    roomWrapper?.addEventListener("scroll", onScroll);
     return () => {
-      observer.disconnect();
+      roomWrapper?.removeEventListener("scroll", onScroll);
     };
   }, []);
-  const toggleDescriptionVisibility = () => {
-    if (!isRoomDescriptionVisible) {
-      setIsToggleableRoomDescriptionHidden(!isToggleableRoomDescriptionHidden);
-    }
-  };
-  let roomDescription = useIndex.eav(props.entityID, "room/description");
+
   let roomName: Fact<"room/name"> | Fact<"member/name"> | null = null;
   let sharedRoomName = useIndex.eav(props.entityID, "room/name");
   let memberRoomName = useIndex.eav(props.entityID, "member/name");
@@ -104,38 +97,32 @@ function RoomHeader(props: {
     roomName = memberRoomName;
   } else roomName = sharedRoomName;
 
-  let descriptionRef = useRef<null | HTMLDivElement>(null);
-
-  let roomType = useIndex.eav(props.entityID, "room/type");
-
   let { authorized } = useMutations();
 
   return (
     <>
       <div
-        className="sticky top-0 z-10 bg-background pt-2 sm:pt-4"
+        className="roomHeader sticky top-0 z-20 bg-background pt-2 sm:pt-4"
         ref={titleRef}
       >
-        <div className="flex justify-between text-lg font-bold text-grey-35">
+        <div className="roomTitle flex justify-between text-lg font-bold text-grey-35">
           <p
-            className={
-              !isRoomDescriptionVisible ? "mb-2 cursor-pointer" : "mb-2"
-            }
-            onClick={toggleDescriptionVisibility}
+            className={!descriptionOpen ? "mb-2 cursor-pointer" : "mb-2"}
+            onClick={() => setDescriptionOpen(!descriptionOpen)}
           >
             {roomName?.value}
           </p>
           {authorized && (
-            <div className="roomButtonWrapper mt-[2px]">
-              {isRoomDescriptionVisible ||
-              !isToggleableRoomDescriptionHidden ? (
+            <div className="roomOptionsWrapper mt-[2px]">
+              {descriptionOpen || scrolledTop ? (
                 <RoomOptions entityID={props.entityID} />
               ) : (
                 <button
                   onClick={() =>
-                    descriptionRef?.current?.scrollIntoView({
+                    document.getElementById("room-wrapper")?.scrollTo({
+                      top: 0,
+                      left: 0,
                       behavior: "smooth",
-                      block: "end",
                     })
                   }
                 >
@@ -145,80 +132,73 @@ function RoomHeader(props: {
             </div>
           )}
         </div>
-
-        {!isRoomDescriptionVisible && !isToggleableRoomDescriptionHidden && (
-          <>
-            <div
-              id="roomDescription2"
-              className="-mt-0 flex flex-col gap-2 pb-2"
-            >
-              {roomDescription?.value && (
-                <RenderedText
-                  style={{
-                    whiteSpace: "pre-wrap",
-                    fontFamily: "inherit",
-                    width: "100%",
-                  }}
-                  className="pb-1 text-sm text-grey-35"
-                  text={roomDescription?.value || ""}
-                />
-              )}
-
-              {props.reactions.length > 0 ? (
-                <div className="flex justify-between gap-2">
-                  {roomType?.value === "collection" ? (
-                    <FilterByReactions
-                      reactions={props.reactions}
-                      filters={props.filters}
-                      setFilters={props.setFilters}
-                    />
-                  ) : null}
-                </div>
-              ) : null}
-            </div>
-          </>
-        )}
       </div>
-      {/* allow expanded toggle if description OR reactions for filtering */}
-      {roomDescription?.value || props.reactions.length > 0 ? (
-        <div
-          id="roomDescription"
-          className="-mt-1 flex flex-col gap-2 pb-2"
-          ref={descriptionRef}
-        >
-          {roomDescription?.value && (
-            <RenderedText
-              className="text-sm text-grey-35"
-              id="roomDescriptionY"
-              style={{
-                whiteSpace: "pre-wrap",
-                fontFamily: "inherit",
-                width: "100%",
-              }}
-              text={roomDescription?.value || ""}
-            />
-          )}
-          {props.reactions.length > 0 ? (
-            <div className="flex justify-between gap-2">
-              {roomType?.value === "collection" ? (
-                <FilterByReactions
-                  reactions={props.reactions}
-                  filters={props.filters}
-                  setFilters={props.setFilters}
-                />
-              ) : null}
-            </div>
-          ) : null}
-        </div>
-      ) : null}
 
-      <hr
-        className="sticky z-10 mb-1 -mt-1 text-grey-80"
-        style={{ top: `${height}px` }}
-      />
+      <div
+        className={` z-10 bg-background pb-3 ${
+          descriptionOpen ? "sticky" : ""
+        }`}
+        style={{ top: `${titleHeight}px` }}
+        ref={descriptionRef}
+      >
+        <RoomDescription
+          entityID={props.entityID}
+          reactions={props.reactions}
+          filters={props.filters}
+          setFilters={props.setFilters}
+        />
+      </div>
+      <div
+        className="sticky z-10 mb-3 bg-background  "
+        style={
+          descriptionOpen
+            ? { top: `calc(${titleHeight}px + ${descriptionHeight}px)` }
+            : { top: `${titleHeight}px` }
+        }
+      >
+        <Divider />
+      </div>
     </>
   );
 }
+
+const RoomDescription = (props: {
+  reactions: string[];
+  filters: Filters;
+  setFilters: (f: (old: Filters) => Filters) => void;
+  entityID: string;
+}) => {
+  let roomDescription = useIndex.eav(props.entityID, "room/description");
+  let roomType = useIndex.eav(props.entityID, "room/type");
+
+  return (
+    <div id="roomDescription" className="flex flex-col gap-2 ">
+      {roomDescription?.value && (
+        <RenderedText
+          className="text-sm text-grey-35"
+          id="roomDescriptionY"
+          style={{
+            whiteSpace: "pre-wrap",
+            fontFamily: "inherit",
+            width: "100%",
+          }}
+          text={roomDescription?.value || ""}
+        />
+      )}
+      {props.reactions.length > 0 ? (
+        <div className="flex justify-between gap-2">
+          {roomType?.value === "collection" ? (
+            <FilterByReactions
+              reactions={props.reactions}
+              filters={props.filters}
+              setFilters={props.setFilters}
+            />
+          ) : null}
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
 function RoomOptions(props: { entityID: string | null }) {
   const [isRoomEditOpen, setIsRoomEditOpen] = useState(false);
