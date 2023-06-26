fb5f11762c5a5659baf4fe8a6b22379b16d0e2e9
Jared Pereira <jared@awarm.space>
2023-06-26 18:03:37 -0400

    add spaces in studios to supabase

--- supabase/migrations/20230616040205_add_studios.sql
+++ supabase/migrations/20230616040205_add_studios.sql
@@ -39,3 +39,31 @@ alter table "public"."studios" add constraint "studios_creator_fkey" FOREIGN KEY
 alter table "public"."studios" validate constraint "studios_creator_fkey";
 
 alter table "public"."studios" add constraint "studios_do_id_key" UNIQUE using index "studios_do_id_key";
+
+
+create table "public"."spaces_in_studios" (
+    "studio" uuid not null,
+    "space" text not null
+);
+
+
+alter table "public"."spaces_in_studios" enable row level security;
+
+CREATE UNIQUE INDEX spaces_in_studios_pkey ON public.spaces_in_studios USING btree (studio, space);
+
+alter table "public"."spaces_in_studios" add constraint "spaces_in_studios_pkey" PRIMARY KEY using index "spaces_in_studios_pkey";
+
+alter table "public"."spaces_in_studios" add constraint "spaces_in_studios_space_fkey" FOREIGN KEY (space) REFERENCES space_data(do_id) ON DELETE CASCADE not valid;
+
+alter table "public"."spaces_in_studios" validate constraint "spaces_in_studios_space_fkey";
+
+alter table "public"."spaces_in_studios" add constraint "spaces_in_studios_studio_fkey" FOREIGN KEY (studio) REFERENCES studios(id) ON DELETE CASCADE not valid;
+
+alter table "public"."spaces_in_studios" validate constraint "spaces_in_studios_studio_fkey";
+
+create policy "Enable read access for all users"
+on "public"."members_in_studios"
+as permissive
+for select
+to public
+using (true);
