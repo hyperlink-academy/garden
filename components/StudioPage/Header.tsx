b2ce8ab75127739f2be9980de70b294af9586479
celine <celine.park.94@GMAIL.COM>
2023-07-12 14:27:22 -0400

    Made studio header a little more mobile friendly

--- components/StudioPage/Header.tsx
+++ components/StudioPage/Header.tsx
@@ -12,34 +12,44 @@ export function StudioHeader(props: {
   let { data } = useStudioData(props.id);
   let { session } = useAuth();
   return (
-    <div className="studioHeader flex flex-col gap-4">
-      <div className="studioTitle flex flex-row items-start justify-between gap-4">
-        <h1>{data?.name}</h1>
-        {/* TODO - replace this with updated 'authorized' in useMutations() */}
-        {data?.members_in_studios.find(
-          (m) => m.member === session?.user?.id
-        ) && (
-          <div className="flex flex-row items-center gap-2">
-            <AddSpace id={props.id} />
-            <StudioOptionsMenu id={props.id} />
-          </div>
-        )}
+    <div className="studioHeader flex flex-col gap-8">
+      <div className=" flex flex-col gap-2">
+        <div className="studioTitle flex flex-row items-start justify-between gap-4">
+          <h1>{data?.name}</h1>
+          {/* TODO - replace this with updated 'authorized' in useMutations() */}
+          {data?.members_in_studios.find(
+            (m) => m.member === session?.user?.id
+          ) && (
+            <div className="flex flex-row items-center gap-2">
+              <div className="hidden sm:block">
+                <AddSpace id={props.id} />
+              </div>
+              <StudioOptionsMenu id={props.id} />
+            </div>
+          )}
+        </div>
+        <p className="studioDescription text-lg">{data?.description}</p>
+        <div className="block sm:hidden ">
+          <AddSpace id={props.id} />
+        </div>
       </div>
-      <p className="studioDescription text-lg">{data?.description}</p>
+
       <div className="studioNav flex flex-col">
-        <div className="flex gap-2">
-          <Tab
-            view={props.view}
-            setView={props.setView}
-            value="posts"
-            display="Posts"
-          />
-          <Tab
-            view={props.view}
-            setView={props.setView}
-            value="spaces"
-            display="Spaces"
-          />
+        <div className="flex justify-between gap-2">
+          <div className="flex gap-2">
+            <Tab
+              view={props.view}
+              setView={props.setView}
+              value="posts"
+              display="Posts"
+            />
+            <Tab
+              view={props.view}
+              setView={props.setView}
+              value="spaces"
+              display="Spaces"
+            />
+          </div>
           <Tab
             view={props.view}
             setView={props.setView}
