e8ddb729f4ea9e532e9ffef9b2f6ad720974e2c6
Jared Pereira <jared@awarm.space>
2023-07-14 16:10:18 -0400

    debounce pwa rerouting

--- pages/pwa.tsx
+++ pages/pwa.tsx
@@ -5,9 +5,13 @@ import { useAuth } from "hooks/useAuth";
 export default function PWAHomepage() {
   let { session } = useAuth();
   useEffect(() => {
-    if (session.session?.username)
-      Router.push("/s/" + session.session.username);
-    else Router.push("/");
+    let timeout = setTimeout(() => {
+      console.log(session);
+      if (session.session?.username)
+        Router.push("/s/" + session.session.username);
+      else Router.push("/");
+    }, 500);
+    return () => clearTimeout(timeout);
   }, [session]);
   return null;
 }
