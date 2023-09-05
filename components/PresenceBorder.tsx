c9ef3ef66768774154e2c866822455aedc3f17cd
Jared Pereira <jared@awarm.space>
2023-09-05 17:22:30 -0400

    animate presence some more

--- components/PresenceBorder.tsx
+++ components/PresenceBorder.tsx
@@ -2,7 +2,11 @@ import { db } from "hooks/useReplicache";
 import { useSpring, animated, useTransition } from "@react-spring/web";
 
 export const PresenceBorder = (props: { entityID: string }) => {
-  let present = db.useReference(props.entityID, "presence/on-card");
+  let present = db
+    .useReference(props.entityID, "presence/on-card")
+    .sort((a, b) => (a.lastUpdated < b.lastUpdated ? -1 : 0))
+    .map((p, index) => ({ ...p, index }))
+    .reverse();
   let presences = useTransition(present, {
     keys: (p) => p.entity,
     from: { top: 32.0 },
@@ -14,10 +18,10 @@ export const PresenceBorder = (props: { entityID: string }) => {
   });
   return (
     <>
-      <div className="absolute right-4 -top-6 flex flex-row-reverse">
+      <div className="absolute -top-6 right-4 flex flex-row-reverse">
         {presences((style, p) => (
           <animated.span className="absolute" style={style}>
-            <Identity entityID={p.entity} />
+            <Identity entityID={p.entity} index={p.index} />
           </animated.span>
         ))}
       </div>
@@ -25,16 +29,18 @@ export const PresenceBorder = (props: { entityID: string }) => {
   );
 };
 
-const Identity = (props: { entityID: string }) => {
+const Identity = (props: { entityID: string; index: number }) => {
   let memberEntity = db.useEntity(props.entityID, "presence/client-member");
   let name = db.useEntity(memberEntity?.value.value || null, "member/name");
   let color = db.useEntity(memberEntity?.value.value || null, "member/color");
+  let style = useSpring({ right: props.index * 32 });
+
   return (
-    <span
-      className="-ml-1.5 rounded-t-md px-1.5 py-1 text-white"
-      style={{ backgroundColor: color?.value }}
+    <animated.span
+      className="relative rounded-t-md px-1.5 py-1 text-white"
+      style={{ ...style, backgroundColor: color?.value }}
     >
       {name?.value}
-    </span>
+    </animated.span>
   );
 };
