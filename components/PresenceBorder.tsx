53c65c2e2d1d2d8667890cbc77702307a360c098
Jared Pereira <jared@awarm.space>
2023-09-05 15:39:35 -0400

    add super simple member tabs

--- components/PresenceBorder.tsx
+++ components/PresenceBorder.tsx
@@ -4,14 +4,28 @@ export const PresenceBorder = (props: {
   children: React.ReactNode;
   entityID: string;
 }) => {
-  return <>{props.children}</>;
+  let present = db.useReference(props.entityID, "presence/on-card");
+  return (
+    <>
+      <div className="absolute right-4 -top-6 flex flex-row-reverse">
+        {present.map((p) => (
+          <Identity entityID={p.entity} key={p.id} />
+        ))}
+      </div>
+      {props.children}
+    </>
+  );
 };
 
 const Identity = (props: { entityID: string }) => {
   let memberEntity = db.useEntity(props.entityID, "presence/client-member");
   let name = db.useEntity(memberEntity?.value.value || null, "member/name");
+  let color = db.useEntity(memberEntity?.value.value || null, "member/color");
   return (
-    <span className="rounded-t-md bg-accent-blue px-1 py-1 text-white">
+    <span
+      className="-ml-1.5 rounded-t-md px-1.5 py-1 text-white"
+      style={{ backgroundColor: color?.value }}
+    >
       {name?.value}
     </span>
   );
