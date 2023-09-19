f1b5ff378cb8a62e9005834ca0ce387712f355b6
Jared Pereira <jared@awarm.space>
2023-09-18 21:05:00 -0400

    don't use swr for useSubscribe

--- hooks/useSubscribe.ts
+++ hooks/useSubscribe.ts
@@ -1,59 +1,14 @@
-import type { Replicache } from "replicache";
+import { useSubscribe as useSubscribeRepl } from "replicache-react";
 import type { ReadonlyJSONValue, ReadTransaction } from "replicache";
-import { useContext, useEffect } from "react";
-import useSWRImmutable from "swr/immutable";
-import { atom } from "jotai";
+import { useContext } from "react";
 import { ReplicacheContext } from "./useReplicache";
-
-// We wrap all the callbacks in a `unstable_batchedUpdates` call to ensure that
-// we do not render things more than once over all of the changed subscriptions.
-//
-//
-let hasPendingCallback = false;
-let callbacks: (() => void)[] = [];
-
-function doCallback() {
-  const cbs = callbacks;
-  callbacks = [];
-  hasPendingCallback = false;
-  for (const callback of cbs) {
-    callback();
-  }
-}
-
-export let suspenseAtom = atom(true);
 
 export function useSubscribe<R extends ReadonlyJSONValue>(
   query: (tx: ReadTransaction) => Promise<R>,
   def: R,
   deps: Array<unknown> = [],
-  key: string
+  _key: string
 ): R {
   let rep = useContext(ReplicacheContext)?.rep;
-  let { data, mutate } = useSWRImmutable(
-    rep ? key + rep.name : null,
-    () => {
-      if (!rep) {
-        return;
-      }
-      return rep.query(query);
-    },
-    {}
-  );
-  useEffect(() => {
-    if (!rep) {
-      return;
-    }
-
-    return rep.subscribe(query, {
-      onData: (data: R) => {
-        callbacks.push(() => mutate(data));
-        if (!hasPendingCallback) {
-          void Promise.resolve().then(doCallback);
-          hasPendingCallback = true;
-        }
-      },
-    });
-  }, [rep, ...deps]);
-  return data || def;
+  return useSubscribeRepl(rep, query, def, deps);
 }
