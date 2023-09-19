9a98931fb226b8c42dac93f4db7d30a56f305c9d
Jared Pereira <jared@awarm.space>
2023-09-18 22:45:28 -0400

    add email confirmation page

---
+++ pages/confirmEmail.tsx
@@ -0,0 +1,25 @@
+import { useAuth } from "hooks/useAuth";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+
+export default function ConfirmEmailPage() {
+  let router = useRouter();
+  let { confirmationURL } = router.query;
+  let { session } = useAuth();
+  let debounceInteveral = useRef<number | null>(null);
+  useEffect(() => {
+    if (debounceInteveral.current)
+      window.clearTimeout(debounceInteveral.current);
+    debounceInteveral.current = window.setTimeout(async () => {
+      if (session.loggedIn) {
+        if (session.session?.username)
+          return router.push(`/s/${session?.session.username}`);
+        else return router.push("/setup");
+      }
+      if (confirmationURL) {
+        return window.location.replace(confirmationURL as string);
+      }
+    }, 500);
+  }, [session, confirmationURL]);
+  return <></>;
+}
