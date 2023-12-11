9558c4756411b8f3df3cc7d584bceefe81110ad0
Jared Pereira <jared@awarm.space>
2023-12-11 17:04:48 -0500

    share space_data queries and get member_data

--- backend/routes/get_space_data.ts
+++ backend/routes/get_space_data.ts
@@ -10,7 +10,7 @@ export const get_space_data_route = makeRoute({
     const supabase = createClient(env, { auth: { persistSession: false } });
     let { data } = await supabase
       .from("space_data")
-      .select(query)
+      .select(space_data_query)
       .eq("do_id", msg.spaceID)
       .single();
 
@@ -28,7 +28,7 @@ export const get_space_data_by_name_route = makeRoute({
     const supabase = createClient(env, { auth: { persistSession: false } });
     let { data } = await supabase
       .from("space_data")
-      .select(query)
+      .select(space_data_query)
       .eq("owner.username", msg.username)
       .eq("name", msg.spaceName)
       .single();
@@ -40,8 +40,11 @@ export const get_space_data_by_name_route = makeRoute({
   },
 });
 
-const query =
-  `*, owner:identity_data!space_data_owner_fkey!inner(*), spaces_in_studios(*, studios(do_id, name))` as const;
+export const space_data_query = `*,
+  owner:identity_data!space_data_owner_fkey!inner(*),
+  spaces_in_studios(*, studios(do_id, name)),
+  members_in_spaces(*)
+` as const;
 
 export type SpaceData = Extract<
   Awaited<ReturnType<typeof get_space_data_route.handler>>,
