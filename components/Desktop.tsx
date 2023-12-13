452c9ee572e1f46a8a4d69207fde8b48cd11c0b8
Jared Pereira <jared@awarm.space>
2023-12-13 13:29:33 -0500

    clamp desktop drop values

--- components/Desktop.tsx
+++ components/Desktop.tsx
@@ -58,8 +58,11 @@ export const Desktop = (props: { entityID: string }) => {
       if (!rect || !droppableRect.current || !rep || data.type === "room")
         return;
       let newPosition = {
-        y: snap(rect.top - droppableRect.current?.top),
-        x: snap(rect.left - droppableRect.current.left),
+        y: Math.max(snap(rect.top - droppableRect.current?.top), 0),
+        x: Math.min(
+          Math.max(0, snap(rect.left - droppableRect.current.left)),
+          320
+        ),
       };
 
       switch (data.type) {
