233191f3c0563a4348936c9b5c3bcaa92b7a0196
Jared Pereira <jared@awarm.space>
2024-01-09 17:42:00 -0500

    add members_in_studios to space_data

--- backend/routes/get_space_data.ts
+++ backend/routes/get_space_data.ts
@@ -60,7 +60,7 @@ export const get_space_data_by_id_route = makeRoute({
 
 export const space_data_query = `*,
   owner:identity_data!space_data_owner_fkey!inner(*),
-  spaces_in_studios(*, studios(do_id, name)),
+  spaces_in_studios(*, studios(do_id, name, members_in_studios(member))),
   members_in_spaces(*),
   user_space_unreads(*)
 ` as const;
