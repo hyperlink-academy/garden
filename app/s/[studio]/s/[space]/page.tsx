05043d8ccbf858bf6d89577e3c43f2e2e047ac83
Jared Pereira <jared@awarm.space>
2024-01-18 01:04:22 -0500

    add metadata for space pages

--- app/s/[studio]/s/[space]/page.tsx
+++ app/s/[studio]/s/[space]/page.tsx
@@ -4,6 +4,16 @@ import { SpaceProvider } from "components/ReplicacheProvider";
 import { PageHeightContainer } from "components/PageHeightContainer";
 import { Space } from "components/Space";
 
+export async function generateMetadata(props: {
+  params: { space: string; studio: string };
+}) {
+  let data = await workerAPI(WORKER_URL, "get_space_data_by_name", {
+    spaceName: props.params.space,
+    username: props.params.studio,
+  });
+  return { title: data.data?.display_name || "404 Space Not Found" };
+}
+
 export default async function SpacePage(props: {
   params: { space: string; studio: string };
 }) {
