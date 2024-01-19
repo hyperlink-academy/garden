ac149abbf966964670ce84663975d79c6daca3fb
Jared Pereira <jared@awarm.space>
2024-01-18 22:26:16 -0500

    tweak viewport size difference

--- components/PageHeightContainer.tsx
+++ components/PageHeightContainer.tsx
@@ -12,7 +12,7 @@ export function PageHeightContainer(props: { children: React.ReactNode }) {
   return (
     <>
       <animated.div
-        style={difference > 100 ? heightSpring : undefined}
+        style={difference > 10 ? heightSpring : undefined}
         className="spacecontent max-w-screen-xl relative mx-auto flex h-screen w-full grow overflow-y-hidden pb-2  sm:pb-4 sm:pt-2"
       >
         {props.children}
