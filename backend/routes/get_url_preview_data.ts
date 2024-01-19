68c6e9f54c0ff81fcd14ffb5b11baca294096605
Jared Pereira <jared@awarm.space>
2024-01-18 20:07:33 -0500

    add logo and image to get_url_preview_data

--- backend/routes/get_url_preview_data.ts
+++ backend/routes/get_url_preview_data.ts
@@ -1,12 +1,29 @@
 import { z } from "zod";
 import { Bindings } from "backend";
 import { makeRoute } from "backend/lib/api";
+import { createClient } from "backend/lib/supabase";
 
 let expectedAPIResponse = z.object({
   data: z.object({
-    description: z.string().optional(),
-    author: z.string().optional(),
-    title: z.string().optional(),
+    description: z.string().optional().nullable(),
+    author: z.string().optional().nullable(),
+    title: z.string().optional().nullable(),
+    image: z
+      .object({
+        url: z.string(),
+        width: z.number(),
+        height: z.number(),
+      })
+      .nullable()
+      .optional(),
+    logo: z
+      .object({
+        url: z.string(),
+        width: z.number(),
+        height: z.number(),
+      })
+      .nullable()
+      .optional(),
   }),
 });
 
@@ -16,15 +33,22 @@ export const get_url_preview_data_route = makeRoute({
     url: z.string(),
   }),
   handler: async (msg, env: Bindings) => {
+    let supabase = createClient(env);
     let response = await fetch(`https://pro.microlink.io/?url=${msg.url}`, {
       headers: {
         "x-api-key": env.MICROLINK_API_KEY,
       },
     });
-    let result = expectedAPIResponse.safeParse(await response.json());
+
+    let responseJSON = await response.json();
+    await supabase
+      .from("debug_logs")
+      .insert({ spaceID: env.id, data: responseJSON });
+
+    let result = expectedAPIResponse.safeParse(responseJSON);
     if (!result.success)
       return {
-        data: { success: false },
+        data: { success: false, responseJSON } as const,
       };
     let data = result.data.data;
     return {
@@ -33,7 +57,9 @@ export const get_url_preview_data_route = makeRoute({
         description: data.description,
         author: data.author,
         title: data.title,
+        image: data.image,
+        logo: data.logo,
       },
-    };
+    } as const;
   },
 });
