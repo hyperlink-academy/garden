bf5dfd3b046cdb87d5d12be5170fd4c07744d636
celine <celine.park.94@GMAIL.COM>
2024-01-18 23:10:55 -0500

    added styling for link previews in card

--- components/LinkPreview.tsx
+++ components/LinkPreview.tsx
@@ -1,18 +1,34 @@
 import { workerAPI } from "backend/lib/api";
 import { db } from "hooks/useReplicache";
+import { useIsMobile } from "hooks/utils";
 import useSWR from "swr";
+
 export const LinkPreview = (props: { entityID: string }) => {
   let data = db.useEntity(props.entityID, "link-preview/data");
-  return (
-    <div className="w-full border p-2">
-      {data?.value.title}
-      {data?.value.description}
-      <a href={data?.value.url}>link</a>
-      {data?.value.image && (
-        <img src={data.value.image.url} className="w-full" />
-      )}
+  let isMobile = useIsMobile();
 
-      {data?.value.logo && <img src={data.value.logo.url} className="w-full" />}
-    </div>
+  return (
+    <a href={data?.value.url}>
+      <div className=" flex h-[86px] w-full gap-3 overflow-hidden rounded-md border border-accent-blue hover:bg-bg-blue sm:h-[120px] sm:gap-4">
+        {/* 
+        {data?.value.image && (
+          <img src={data.value.image.url} className="w-full" />
+        )} */}
+
+        {data?.value.logo && (
+          <img
+            src={data.value.logo.url}
+            height={isMobile ? 86 : 120}
+            width={isMobile ? 86 : 120}
+            alt="a website logo"
+            className="shrink-0"
+          />
+        )}
+        <div className="flex flex-col gap-2 py-1 pr-1 sm:py-2 sm:pr-2">
+          <p className="font-bold text-accent-blue">{data?.value.title}</p>
+          <p className="text-sm text-grey-55">{data?.value.description}</p>
+        </div>
+      </div>
+    </a>
   );
 };
