d5e9c46829e15551111a033cbc003ffd03ce3945
Jared Pereira <jared@awarm.space>
2024-01-20 17:53:44 -0500

    fix ismember logic to handle studios

--- backend/SpaceDurableObject/lib/isMember.ts
+++ backend/SpaceDurableObject/lib/isMember.ts
@@ -4,10 +4,17 @@ import { Env } from "..";
 export async function isUserMember(env: Env, userID: string) {
   let supabase = createClient(env.env);
   let { data: isMember } = await supabase
-    .from("members_in_spaces")
-    .select("*")
-    .eq("space_do_id", env.id)
-    .eq("member", userID)
+    .from("identity_data")
+    .select(
+      "members_in_spaces(space_data!inner(do_id)), members_in_studios(studios!inner(do_id))"
+    )
+    .eq("id", userID)
+    .eq("members_in_spaces.space_data.do_id", env.id)
+    .eq("members_in_studios.studios.do_id", env.id)
     .single();
-  return !!isMember;
+
+  return (
+    !!isMember?.members_in_spaces.length > 0 ||
+    !!isMember?.members_in_studios.length > 0
+  );
 }
