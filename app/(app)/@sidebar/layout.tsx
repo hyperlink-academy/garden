39f2e9b2ae20cc06bcd0322a0ceff1509570e830
Jared Pereira <jared@awarm.space>
2024-02-27 14:06:59 -0500

    make sidebar fixed width

--- app/(app)/@sidebar/layout.tsx
+++ app/(app)/@sidebar/layout.tsx
@@ -1,3 +1,3 @@
 export default function Sidebar(props: { children: React.ReactNode }) {
-  return <div className="h-full border">{props.children}</div>;
+  return <div className="h-full w-96 border">{props.children}</div>;
 }
