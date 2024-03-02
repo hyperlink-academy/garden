89065127d00979ecd0aa83d6f3f0eb0ecd134ce1
celine <thisiscelinepark@gmail.com>
2024-03-02 00:50:57 -0500

    did all the easy styling for the studio sidebar

--- app/(app)/@sidebar/studio/[studio_id]/page.tsx
+++ app/(app)/@sidebar/studio/[studio_id]/page.tsx
@@ -4,6 +4,7 @@ import { getStudioPageData } from "app/(app)/studio/[studio_id]/page";
 import Link from "next/link";
 import { supabaseServerClient } from "supabase/server";
 import { TabsList } from "./StudioTabs";
+import { Divider } from "components/Layout";
 
 export default async function StudioSidebar(props: {
   params: { studio_id: string };
@@ -16,29 +17,43 @@ export default async function StudioSidebar(props: {
 
   return (
     <>
-      <div className="flex flex-row">
-        <Link
-          href={
-            session.data.user
-              ? `/s/${session.data.user.user_metadata.username}`
-              : "/"
-          }
-        >
-          h/
-        </Link>
-        {data?.data.name}
-      </div>
-      <div className="flex w-full flex-col gap-2">
-        <div className="flex flex-col gap-0">
-          <h3
-            style={{ wordBreak: "break-word" }} //no tailwind equiv - need for long titles to wrap
-          ></h3>
+      <div className="sidebarStudio flex h-full flex-col items-stretch gap-1">
+        <div className="flex items-center justify-between px-3">
+          <Link
+            href={
+              session.data.user
+                ? `/s/${session.data.user.user_metadata.username}`
+                : "/"
+            }
+            className="sidebarBreadcrumb text-grey-55 flex shrink-0 flex-row text-sm"
+          >
+            <div className="flex gap-1">
+              <div className="font-bold">h</div>
+              <div className="font-bold">/</div>
+            </div>
+          </Link>
         </div>
-        <TabsList
-          className="StudioTabs flex flex-col gap-2 "
-          data={data.data}
-          isAdmin={isAdmin}
-        />
+        <div className="sidebarStudioName shrink-0 flex-row px-3 text-lg font-bold">
+          {data?.data.name}
+        </div>
+        <div className="px-3">
+          <input
+            placeholder="search studio (ctrl/⌘ K)"
+            className="false w-full px-2 py-1 text-sm outline-none"
+          />
+        </div>
+        <div className="pb-1 pt-2">
+          <Divider />
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <div className="flex flex-col gap-0.5 px-3">
+            <TabsList
+              className="StudioTabs flex flex-col gap-2 "
+              data={data.data}
+              isAdmin={isAdmin}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
