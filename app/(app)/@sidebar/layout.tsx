4567882d3638f8cbf3937199db6bed5eca2f9817
Jared Pereira <jared@awarm.space>
2024-03-13 16:20:47 -0400

    tiny lil animated thing

--- app/(app)/@sidebar/layout.tsx
+++ app/(app)/@sidebar/layout.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import { useSpring, animated } from "@react-spring/web";
+import { useState } from "react";
+
 export default function Sidebar(props: { children: React.ReactNode }) {
+  let [open, setOpen] = useState(true);
+  let spring = useSpring({
+    width: open ? 256 : 16,
+    config: { duration: 200 },
+  });
   return (
-    <div className="lightBorder flex w-64 shrink-0 flex-col overflow-hidden bg-white ">
-      <div className="h-full w-full overflow-x-hidden  py-3 ">
-        {props.children}
+    <animated.div
+      style={spring}
+      className="lightBorder flex w-64 shrink-0 flex-col overflow-hidden bg-white "
+    >
+      <div className="w-64">
+        <div className="h-full w-full overflow-x-hidden  py-3 ">
+          <button onClick={() => setOpen(!open)}>toggle</button>
+          {props.children}
+        </div>
       </div>
-    </div>
+    </animated.div>
   );
 }
