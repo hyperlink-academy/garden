07a5387f05039f2f9d19c96e5915acfe3d0b09f6
Jared Pereira <jared@awarm.space>
2024-03-14 16:49:52 -0400

    add space provider to studio sidebar

--- app/(app)/@sidebar/studio/[studio_id]/page.tsx
+++ app/(app)/@sidebar/studio/[studio_id]/page.tsx
@@ -2,6 +2,7 @@
 import { getStudioPageData } from "app/(app)/studio/[studio_id]/page";
 import { supabaseServerClient } from "supabase/server";
 import { StudioSidebarContent } from "./StudioSidebar";
+import { SpaceProvider } from "components/ReplicacheProvider";
 
 export default async function StudioSidebar(props: {
   params: { studio_id: string };
@@ -14,8 +15,11 @@ export default async function StudioSidebar(props: {
   let isAdmin = data.data.creator === session?.data.user?.id;
 
   return (
-    <>
+    <SpaceProvider
+      id={data.data.do_id}
+      data={{ studio_id: data.data.id, space_id: undefined }}
+    >
       <StudioSidebarContent isAdmin={isAdmin} data={data.data} />
-    </>
+    </SpaceProvider>
   );
 }
