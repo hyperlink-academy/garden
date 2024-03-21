0dae28bc6fe16fcb12129ec907b583f7897420eb
Brendan Schlagel <brendan.schlagel@gmail.com>
2024-03-21 18:26:02 -0400

    style tweaks for docs page

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,4 +1,6 @@
 import { HelpDocs } from "components/HelpCenter";
+import { GoBackToPageLined } from "components/Icons";
+import Link from "next/link";
 
 export const metadata = {
   title: "Hyperlink Docs",
@@ -6,10 +8,18 @@ export const metadata = {
 
 export default function DocsPage() {
   return (
-    <>
-      <div className="grid-rows-max mx-auto grid gap-4">
-        <HelpDocs />
+    <div className="docs mx-auto flex max-w-3xl flex-col gap-8 px-4 py-4 md:px-8 md:py-8">
+      <div className="flex flex-col gap-2 text-center">
+        <h2>Hyperlink Docs</h2>
+        <Link
+          href="/"
+          className="mx-auto flex items-center justify-center gap-2 hover:text-accent-blue"
+        >
+          <GoBackToPageLined />
+          home
+        </Link>
       </div>
-    </>
+      <HelpDocs />
+    </div>
   );
 }
