5ff2dc9d214bec1cecbbdd90a6e9d7af59aab588
Brendan Schlagel <brendan.schlagel@gmail.com>
2024-03-22 16:33:22 -0400

    initial version of hyperlink roadmap in docs

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -10,7 +10,7 @@ export default function DocsPage() {
   return (
     <div className="docs mx-auto flex max-w-3xl flex-col gap-8 px-4 py-4 md:px-8 md:py-8">
       <div className="flex flex-col gap-2 text-center">
-        <h2>Hyperlink Docs</h2>
+        <h1>Hyperlink Docs</h1>
         <Link
           href="/"
           className="mx-auto flex items-center justify-center gap-2 hover:text-accent-blue"
--- components/HelpCenter.tsx
+++ components/HelpCenter.tsx
@@ -2,9 +2,15 @@
 import { Tab } from "@headlessui/react";
 import { useSearchParams } from "next/navigation";
 import { Fragment, useEffect, useState } from "react";
-import { CloseLinedTiny, Settings } from "./Icons";
+import {
+  CloseLinedTiny,
+  DisclosureCollapseTiny,
+  DisclosureExpandTiny,
+  Settings,
+} from "./Icons";
 import { ModalFixedHeight } from "./Layout";
 import { isAppleDevice } from "@react-aria/utils";
+import { Disclosure } from "@headlessui/react";
 
 export const HelpModal = (props: { open: boolean; onClose: () => void }) => {
   return (
@@ -40,7 +46,14 @@ export const HelpDocs = () => {
   useEffect(() => {
     setSelectedIndex(() => {
       return tab
-        ? ["handbook", "shortcuts", "examples", "app"].indexOf(tab as string)
+        ? [
+            "handbook",
+            "shortcuts",
+            "roadmap",
+            "changelog",
+            "examples",
+            "app",
+          ].indexOf(tab as string)
         : 0;
     });
   }, [tab]);
@@ -79,7 +92,20 @@ export const HelpDocs = () => {
               </button>
             )}
           </Tab>
-          {/* <Tab as={Fragment}>
+          <Tab as={Fragment}>
+            {({ selected }) => (
+              <button
+                className={`rounded-md border p-2 ${
+                  selected
+                    ? "border-accent-blue bg-accent-blue text-white"
+                    : "text-black border-grey-80 bg-white hover:bg-bg-blue"
+                }`}
+              >
+                Roadmap
+              </button>
+            )}
+          </Tab>
+          <Tab as={Fragment}>
             {({ selected }) => (
               <button
                 className={`rounded-md border p-2 ${
@@ -91,7 +117,7 @@ export const HelpDocs = () => {
                 Changelog
               </button>
             )}
-          </Tab> */}
+          </Tab>
           <Tab as={Fragment}>
             {({ selected }) => (
               <button
@@ -126,9 +152,12 @@ export const HelpDocs = () => {
           <Tab.Panel>
             <HelpShortcuts />
           </Tab.Panel>
-          {/* <Tab.Panel>
+          <Tab.Panel>
+            <HelpRoadmap />
+          </Tab.Panel>
+          <Tab.Panel>
             <HelpChangelog />
-          </Tab.Panel> */}
+          </Tab.Panel>
           <Tab.Panel>
             <HelpExampleSpaces />
           </Tab.Panel>
@@ -298,6 +327,176 @@ const HelpShortcuts = () => {
   );
 };
 
+const HelpRoadmap = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h2>Themes & Focus Areas</h2>
+      <p className="">
+        Use cases we&apos;re prioritizing; areas where we want Hyperlink to
+        shine:
+      </p>
+      <HelpDisclosureSection>
+        <HelpDisclosure name="Hyperlink for Clubs">
+          <p>
+            Currently running experiments with internet clubs, and we want to
+            make the app even better here — it should give us great ways to
+            bring groups of people together for creative work and learning.
+          </p>
+          <p>Some things this might include:</p>
+          <ul className="flex list-disc flex-col gap-1 pl-4 sm:pl-8">
+            <li>
+              More ways of representing key info: rich text, multimedia cards,
+              etc.
+            </li>
+            <li>
+              Surfacing and sharing activity to keep momentum going; things
+              should feel active and dynamic
+            </li>
+            <li>
+              Making Studios more powerful, e.g. with an activity log or chat
+              and cards at the top level
+            </li>
+            <li>
+              Built-in ways to send a group email / digest, perhaps as a card
+            </li>
+            <li>
+              Customization / theming so groups&apos; Spaces and Studios can
+              better reflect their identity
+            </li>
+            <li>
+              Ways for people to discover and contribute to relevant clubs
+            </li>
+          </ul>
+        </HelpDisclosure>
+        <HelpDisclosure name="Hyperlink for Schools">
+          <p>
+            In the medium term, we want Hyperlink to be the best platform for
+            independent internet-native schools to organize their activity.
+          </p>
+          <p>This might include:</p>
+          <ul className="flex list-disc flex-col gap-1 pl-4 sm:pl-8">
+            <li>
+              Composable structure with Spaces and Studios reflecting more
+              complex hierarchies e.g. of cohorts, departments, and schools
+            </li>
+            <li>More advanced admin tools and settings</li>
+            <li>
+              Things like file uploads to have all learning materials in one
+              place
+            </li>
+            <li>
+              Calendars, scheduled cards, or other ways of working with things
+              through time
+            </li>
+            <li>
+              Other ways we can support novel models for schools — networked,
+              personal, emergent, fractal…
+            </li>
+          </ul>
+        </HelpDisclosure>
+      </HelpDisclosureSection>
+
+      <h2>Features</h2>
+      <p>
+        What we&apos;re working on! To view what we&apos;ve shipped, see our{" "}
+        <strong>changelog</strong>.
+      </p>
+      <p>
+        If you have thoughts on what we should prioritize,{" "}
+        <a
+          className="font-bold text-accent-blue"
+          href="mailto:contact@hyperlink.academy"
+        >
+          please reach out
+        </a>
+        !
+      </p>
+      <div className="flex flex-col gap-2">
+        <h3>Active</h3>
+        <p className="italic">currently in progress</p>
+      </div>
+      <HelpDisclosureSection>
+        <HelpDisclosure name="Global nav & notifications">
+          <p>
+            Refactoring navigation throughout the app to use a consistent
+            sidebar.
+          </p>
+          <p>
+            This is a big one, touching all parts of the app, but will make
+            everything feel more stable <em>and</em>
+            allow us to add a global notification center!
+          </p>
+        </HelpDisclosure>
+        <HelpDisclosure name="Card multiselect and actions">
+          <p>A way to select many cards at once, and do things with them.</p>
+          <p>
+            Card actions may include changing color or reactions, dragging in
+            canvas, or moving cards to other rooms.
+          </p>
+        </HelpDisclosure>
+      </HelpDisclosureSection>
+      <div className="flex flex-col gap-2">
+        <h3>Planned</h3>
+        <p className="italic">happening soon, with high likelihood</p>
+      </div>
+      <HelpDisclosureSection>
+        <HelpDisclosure name="Block-based card editor">
+          <p>
+            New card editor, allowing for more flexible, modular card editing.
+          </p>
+          <p>
+            This will enable interspersing text with images, linked cards, and
+            (eventually) other media.
+          </p>
+        </HelpDisclosure>
+
+        <HelpDisclosure name="Multi-column side scrolling card viewer">
+          <p>
+            Rather than just one room and one card open next to it, you should
+            be able to click a card and have it open in a new column, with
+            arbitrary nesting.
+          </p>
+          <p>
+            This will make it easier to quickly browse collections, navigate
+            nested rooms and cards, and compare things side by side.
+          </p>
+        </HelpDisclosure>
+        <HelpDisclosure name="Private Spaces & Studios">
+          <p>
+            A setting for both Spaces and Studios to make them viewable only to
+            members.
+          </p>
+          <p>
+            Many people prefer to do more intimate work behind closed doors; we
+            want to support that.
+          </p>
+        </HelpDisclosure>
+        <HelpDisclosure name="Resizable canvas">
+          <p>
+            The &quot;canvas&quot; room has a mobile-sized fixed width, which is
+            limiting!
+          </p>
+          <p>
+            We want a way to make it bigger so you can visually organize cards
+            in more flexible ways.
+          </p>
+        </HelpDisclosure>
+        <HelpDisclosure name="Room refactor">
+          <p>
+            There are some changes we want to make to the relationship between
+            rooms, cards, and the sidebar.
+          </p>
+          <p>
+            Things like: adding cards directly to the sidebar, allowing rooms to
+            be linked in cards and other rooms, and making collection and canvas
+            a view toggle rather than a separate room type.
+          </p>
+        </HelpDisclosure>
+      </HelpDisclosureSection>
+    </div>
+  );
+};
+
 const HelpChangelog = () => {
   return (
     <div className="flex flex-col gap-4">
@@ -354,8 +553,8 @@ export const HelpAppInfo = () => {
   return (
     <div className="flex flex-col gap-4">
       <p>
-        Get Hyperlink on your home screen — and get push notifications for
-        activity in your Spaces.
+        Get Hyperlink on your home screen — and push notifications for new
+        activity.
       </p>
       <h4>First, grab your phone 📱</h4>
       <ol className="flex list-decimal flex-col gap-1 pl-8">
@@ -403,5 +602,34 @@ const UnicodeKeyboardKey = (props: { children: React.ReactNode }) => {
         {props.children}
       </code>
     </span>
+  );
+};
+
+const HelpDisclosureSection = (props: { children: React.ReactNode }) => {
+  return (
+    <div className="flex w-full flex-col items-start gap-0 rounded-md bg-white p-2">
+      {props.children}
+    </div>
+  );
+};
+
+const HelpDisclosure = (props: { children: React.ReactNode; name: string }) => {
+  return (
+    <Disclosure
+      as="div"
+      className="flex w-full flex-col gap-2 rounded-md bg-white p-2"
+    >
+      {({ open }) => (
+        <>
+          <Disclosure.Button className="flex w-full items-center justify-between gap-2 rounded-md bg-bg-gold px-4 py-2 text-left hover:bg-grey-90 focus:outline-none focus-visible:ring focus-visible:ring-accent-blue">
+            <span>{props.name}</span>
+            {open ? <DisclosureExpandTiny /> : <DisclosureCollapseTiny />}
+          </Disclosure.Button>
+          <Disclosure.Panel className="rounded-md px-4 py-2 pb-2 pt-4">
+            <div className="flex flex-col gap-4">{props.children}</div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 };
