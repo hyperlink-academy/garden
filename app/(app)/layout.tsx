9bbf2651e006244b71900521fa6f9a0656e8ed30
Jared Pereira <jared@awarm.space>
2024-03-26 13:53:48 -0400

    add higher z-index to sidebar

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -51,7 +51,7 @@ export default function AppLayout(props: {
       id="appLayout"
       className="flex h-full w-full snap-x snap-mandatory scroll-pl-4 flex-row overflow-x-scroll overflow-y-scroll px-2 sm:gap-4 sm:px-8 "
     >
-      <div className="appSidebarWrapper sticky left-0 top-0 z-20 flex items-stretch bg-transparent py-2 sm:w-auto">
+      <div className="appSidebarWrapper sticky left-0 top-0 z-40 flex items-stretch bg-transparent py-2 sm:w-auto">
         {props.sidebar}
       </div>
       <div className="appContentWrapper flex h-full flex-row gap-4 px-2 py-2 sm:gap-4">
