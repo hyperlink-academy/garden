d8dbf5e2022e9ec480f263bb91b3f534f29a0a2e
Jared Pereira <jared@awarm.space>
2024-04-01 12:04:38 -0400

    don't cache userPageData

--- app/(app)/s/[studio]/getUserPageData.ts
+++ app/(app)/s/[studio]/getUserPageData.ts
@@ -2,8 +2,8 @@ import { cache } from "react";
 import { workerAPI } from "backend/lib/api";
 import { WORKER_URL } from "src/constants";
 
-export const getUserPageData = cache(({ studio }: { studio: string }) => {
+export const getUserPageData = ({ studio }: { studio: string }) => {
   return workerAPI(WORKER_URL, "get_identity_data", {
     name: studio,
   });
-});
+};
