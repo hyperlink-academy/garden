a68c1ac4a4818005328d389b786b1cd33dca649c
Jared Pereira <jared@awarm.space>
2024-04-30 23:13:40 -0400

    require user to click button to redirect on confirmEmail

--- app/confirmEmail/page.tsx
+++ app/confirmEmail/page.tsx
@@ -1,3 +1,4 @@
+import { ButtonSecondary } from "components/Buttons";
 import { redirect } from "next/navigation";
 import { supabaseServerClient } from "supabase/server";
 
@@ -12,7 +13,16 @@ export default async function ConfirmEmailPage(props: {
     else return redirect("/setup");
   }
   if (props.searchParams.confirmationURL) {
-    return redirect(props.searchParams.confirmationURL);
+    return (
+      <div className=" -my-4 mx-auto flex h-screen max-w-md flex-col items-center justify-center gap-4 px-3 ">
+        <div className="lightBorder mt-3 flex w-full max-w-md flex-col gap-4 bg-white p-3 sm:p-4">
+          Great! Please click here to confirm your email.
+          <a href={props.searchParams.confirmationURL}>
+            <ButtonSecondary content="Confirm Your Email!" />
+          </a>
+        </div>
+      </div>
+    );
   }
   return null;
 }
